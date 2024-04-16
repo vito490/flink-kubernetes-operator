c276fb47b903154fd7624aa0900c9de64e6c5e5a
059298758 <vito.sammarco1@ibm.com>
2024-04-16 15:11:40 +0200

    utility scripts for topics reset

---
+++ examples/flink-sql-runner-example/utility-scripts/increase-topics-retention.ps1
@@ -0,0 +1,10 @@
+kaf topic add-config NEXI.PRIMO.OUTPUT retention.ms 86400000
+kaf topic add-config NEXI.PRIMO.OUTPUT delete.retention.ms 86400000
+kaf topic add-config NEXI.TEMPI_RISPOSTA retention.ms 86400000
+kaf topic add-config NEXI.TEMPI_RISPOSTA delete.retention.ms 86400000
+kaf topic add-config NEXI.TOTALI retention.ms 86400000
+kaf topic add-config NEXI.TOTALI delete.retention.ms 86400000
+kaf topic add-config NEXI.VALORE_TRANSAZIONI retention.ms 86400000
+kaf topic add-config NEXI.VALORE_TRANSAZIONI delete.retention.ms 86400000
+
+
---
+++ examples/flink-sql-runner-example/utility-scripts/lower-topics-retention.ps1
@@ -0,0 +1,10 @@
+kaf topic add-config NEXI.PRIMO.OUTPUT retention.ms 1000
+kaf topic add-config NEXI.PRIMO.OUTPUT delete.retention.ms 1000
+kaf topic add-config NEXI.TEMPI_RISPOSTA retention.ms 1000
+kaf topic add-config NEXI.TEMPI_RISPOSTA delete.retention.ms 1000
+kaf topic add-config NEXI.TOTALI retention.ms 1000
+kaf topic add-config NEXI.TOTALI delete.retention.ms 1000
+kaf topic add-config NEXI.VALORE_TRANSAZIONI retention.ms 1000
+kaf topic add-config NEXI.VALORE_TRANSAZIONI delete.retention.ms 1000
+
+
